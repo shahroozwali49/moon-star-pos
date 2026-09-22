d916642eccf5500f874235d3e5265e6c12b30192
shahroozwali49 <shahroozwali49@gmail.com>
2026-09-22 18:48:38 +0500

    Consolidate backend imports under root lib

--- app/api/admin/stores/route.js
+++ app/api/admin/stores/route.js
@@ -1,6 +1,6 @@
-import { connectDB } from "../../../lib/mongodb";
-import Store from "../../../lib/models/Store";
-import { requireSuperAdmin } from "../../../lib/permissions";
+import { connectDB } from "../../../../lib/mongodb";
+import Store from "../../../../lib/models/Store";
+import { requireSuperAdmin } from "../../../../lib/permissions";
 
 export async function GET() {
   const auth = await requireSuperAdmin();
