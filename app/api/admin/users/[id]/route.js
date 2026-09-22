c9f6a32fea4aaa92cfbe7e4c3b7991273aa18dd3
shahroozwali49 <shahroozwali49@gmail.com>
2026-09-22 18:01:29 +0500

    Prevent dynamic admin user route build-time evaluation

--- app/api/admin/users/[id]/route.js
+++ app/api/admin/users/[id]/route.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import { connectDB } from "../../../../lib/mongodb";
